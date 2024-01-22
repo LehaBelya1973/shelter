be2b6a678c956f24b3e9d733ecb86977cd30409e
Aleksey Belyaev <lesha.belyaev.73@mail.ru>
2024-01-22 20:27:31 +0300

    Исправил ошибки в models.py, создал миграции

--- dogs/models.py
+++ dogs/models.py
@@ -7,7 +7,7 @@ NULLABLE = {'blank': True, 'null': True}
 class Dog(models.Model):
     name = models.CharField(max_length=250, verbose_name='Кличка')
     category = models.CharField(max_length=100, verbose_name='Порода')
-    phot = models.IntegerField(upload_to='dogs/', **NULLABLE)
+    phot = models.ImageField(upload_to='dogs/', **NULLABLE, verbose_name='Фото')
     birth_day = models.DateField(**NULLABLE, verbose_name='Дата рождения')
 
     def __str__(self):
