7a646bb11dc444113eaaea0fc255404642c49074
Aleksey Belyaev <lesha.belyaev.73@mail.ru>
2024-01-22 20:21:17 +0300

    Создал модель собаки, дописал в settings.py связь с медиа, urls.py

--- dogs/models.py
+++ dogs/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Dog(models.Model):
+    name = models.CharField(max_length=250, verbose_name='Кличка')
+    category = models.CharField(max_length=100, verbose_name='Порода')
+    phot = models.IntegerField(upload_to='dogs/', **NULLABLE)
+    birth_day = models.DateField(**NULLABLE, verbose_name='Дата рождения')
+
+    def __str__(self):
+        return f'{self.name} ({self.category})'
+
+    class Meta:
+        verbose_name = 'собака'
+        verbose_name_plural = 'собаки'
